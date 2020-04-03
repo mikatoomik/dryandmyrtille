463fc3491e8dbb47a01b883f455b8fff457e3e09
mikael autheman <mika@atoomik.fr>
2020-04-03 12:04:03 +0200

    menu_collection_navbar

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,8 +1,15 @@
 class CollectionsController < ApplicationController
   def index
-    @collections = Collection.all
+   @collections = Collection.all
+  #  @collections = ShopifyAPI::CustomCollection.all
   end
    def show
-    @collection = Collection.find(params[:id])
-   end
+    @collections = Collection.all
+    chosen_collection = Collection.find(params[:id])
+    @collection = ShopifyAPI::CustomCollection.find(:all, :params => {:title => "#{chosen_collection.name}"}).first
+    @products = ShopifyAPI::Product.find(:all, :params => {:collection_id => @collection.id})
+   # products = RestClient.get("https://#{ENV['SHOPIFY_API_KEY']}:#{ENV['SHOPIFY_PASSWORD']}@dryandmyrtille.myshopify.com/admin/api/2020-01/products.json")
+   # render json: products
+  end
 end
+
