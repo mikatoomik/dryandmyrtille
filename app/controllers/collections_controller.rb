0e44b42855fe3666b7a99fe46acda17d9ec94c2b
mikael autheman <mika@atoomik.fr>
2020-04-04 10:31:12 +0200

    collections page design

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,11 +5,18 @@ class CollectionsController < ApplicationController
   end
    def show
     @collections = Collection.all
-    chosen_collection = Collection.find(params[:id])
-    @collection = ShopifyAPI::CustomCollection.find(:all, :params => {:title => "#{chosen_collection.name}"}).first
+    @chosen_collection = Collection.find(params[:id])
+    @collection = ShopifyAPI::CustomCollection.find(:all, :params => {:title => "#{@chosen_collection.name}"}).first
     @products = ShopifyAPI::Product.find(:all, :params => {:collection_id => @collection.id})
    # products = RestClient.get("https://#{ENV['SHOPIFY_API_KEY']}:#{ENV['SHOPIFY_PASSWORD']}@dryandmyrtille.myshopify.com/admin/api/2020-01/products.json")
    # render json: products
+  end
+  def update
+    @collection = Collection.find(params[:id])
+    @collection.update(collection_params)
+  end
+  def collection_params
+  params.require(:collection).permit(:name, photos: [])
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'products', to: 'pages#product'
   resources :points, only: [:index]
-  resources :collections, only: [:show, :index]
+  resources :collections, only: [:show, :index, :update]
 end
