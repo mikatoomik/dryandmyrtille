3ce76a7a0f8886e1fb6c7df5bf7800bc965a085a
mikael autheman <mika@atoomik.fr>
2020-04-04 18:19:01 +0200

    design product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'products', to: 'pages#product'
+  # get 'products', to: 'products#index'
+  # get 'products/:id', to: 'products#show'
   resources :points, only: [:index]
   resources :collections, only: [:show, :index, :update]
+  resources :products, only: [:show, :index]
 end
